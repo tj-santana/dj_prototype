e41e243051b41f90465fda1a33a3d7e97a56b94e
Scarface1809 <joao.miguel.santos1809@gmail.com>
2025-10-08 14:55:19 +0100

    feat: main menu

---
+++ src/main_menu.gd
@@ -0,0 +1,21 @@
+extends Control
+class_name MainMenu
+
+@onready var play: Button = %Play
+@onready var settings: Button = %Settings
+@onready var quit: Button = %Quit
+
+func _ready() -> void:
+	play.pressed.connect(_on_play_pressed)
+	settings.pressed.connect(_on_settings_pressed)
+	quit.pressed.connect(_on_quit_pressed)
+
+# Signal handlers
+func _on_play_pressed() -> void:
+	Global.game_controller.change_gui_scene(Refs.PATHS["OVERVIEW"], TransitionSettings.TRANSITION_TYPE.MAIN_MENU_TO_GAME)
+
+func _on_settings_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
