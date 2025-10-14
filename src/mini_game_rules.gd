bf2af3b413d96e6fb2914b03a3f85ca31d7706c8
Scarface1809 <joao.miguel.santos1809@gmail.com>
2025-10-14 03:18:46 +0100

    fix: mini game rules scene #27

--- src/mini_game_rules.gd
+++ src/mini_game_rules.gd
@@ -1,6 +1,5 @@
 extends ComputerScreen
 class_name MiniGameRules
 
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_accept"):
-		change_screen(Refs.PATHS.MINI_GAME)
+func _on_confirm_button_pressed() -> void:
+	change_screen(Refs.PATHS.MINI_GAME)
