503ed61b6a96f2f25d0b8d32b012321cfffb696e
Scarface1809 <joao.miguel.santos1809@gmail.com>
2025-10-14 03:19:42 +0100

    fix: news publishing scene #7

--- src/news_publishing.gd
+++ src/news_publishing.gd
@@ -1,8 +1,5 @@
 extends Control
 class_name NewsPublishing
 
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_accept"):
-		await Global.game_controller.change_gui_scene("", TransitionSettings.TRANSITION_TYPE.MAIN_MENU_TO_GAME)
-		await Global.game_controller.change_2d_scene(Refs.PATHS.FIGHT, TransitionSettings.TRANSITION_TYPE.MAIN_MENU_TO_GAME)
-		pass
+func _on_confirm_button_pressed() -> void:
+	await Global.game_controller.change_scene("", Refs.PATHS.FIGHT, TransitionSettings.TRANSITION_TYPE.MAIN_MENU_TO_GAME)
