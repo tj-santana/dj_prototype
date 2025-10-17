d9c9ec5d37c7d4e25a9e20db1dbcec85847f933c
Tiago Ferreira <tiago.a.p.ferreira@tecnico.ulisboa.pt>
2025-10-17 18:13:10 +0100

    feat: 1st mission bubble
    simple connection to first mission. Needs merging to work

--- src/overview/overview.gd
+++ src/overview/overview.gd
@@ -1,17 +1,12 @@
 extends Node2D
 class_name Overview
 
-# TODO: Add map interactibles, buttons and functions
-# Template:
-#@onready var play: Button = %Play
+@onready var mission: Button = %Mission
 
 func _ready() -> void:
-# Template:
-#	play.pressed.connect(_on_play_pressed)
+	mission.pressed.connect(_on_mission_pressed)
 	pass
 
 
-# Signal handlers
-# Template:
-#func _on_play_pressed() -> void:
-#	Global.game_controller.change_scene("", Refs.PATHS.OVERVIEW, TransitionSettings.TRANSITION_TYPE.MAIN_MENU_TO_GAME)
+func _on_mission_pressed() -> void:
+	await Global.game_controller.change_gui_scene(Refs.PATHS.PORTATIL, TransitionSettings.TRANSITION_TYPE.MAIN_MENU_TO_GAME)
