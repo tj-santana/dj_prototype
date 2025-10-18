4749f1466b6d67ff8ce57b20156561e074f3bb0b
Scarface1809 <joao.miguel.santos1809@gmail.com>
2025-10-18 23:23:30 +0100

    fix: audio resources

--- src/refs.gd
+++ src/refs.gd
@@ -11,7 +11,7 @@ const PATHS = {
 	"FIGHT_GUI": "uid://bvnj6m67ru0po",
 	"BOSS_CORRUPT": "uid://ch1yx31kbmfm8",
 	"BOSS_ANTI_CORRUPT": "uid://bof3ks2cj1btn",
-	"TEEN_PROJECTILE": "uid://3fe5ij0dftxt"
+	"TEEN_PROJECTILE": "uid://3fe5ij0dftxt",
 	"MINI_GAME_RULES": "uid://c76di0uhcydaa",
 	"MINI_GAME": "uid://d0wtpgkfw785g",
 	"NEWS_PUBLISHING": "uid://c20bi27e7pxqp",
