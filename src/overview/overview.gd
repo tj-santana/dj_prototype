7500e321762f4528457872ce1c6576de5cb4695f
Tiago Ferreira <tiagofer2004@gmail.com>
2025-10-18 18:37:53 +0100

    fix: button detection

--- src/overview/overview.gd
+++ src/overview/overview.gd
@@ -3,10 +3,16 @@ class_name Overview
 
 @onready var mission: Button = %Mission
 
-func _ready() -> void:
-	mission.pressed.connect(_on_mission_pressed)
-	pass
 
 
-func _on_mission_pressed() -> void:
+	# Button ditection is not working for som reason.
+	# So intead we calculate if the click was on the area of the button.
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed and mission:
+		var button_rect = Rect2(mission.global_position, mission.size)
+		if button_rect.has_point(event.global_position):
+			_handle_mission_click()
+
+func _handle_mission_click():
 	await Global.game_controller.change_gui_scene(Refs.PATHS.PORTATIL, TransitionSettings.TRANSITION_TYPE.MAIN_MENU_TO_GAME)
