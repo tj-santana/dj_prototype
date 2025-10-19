6f4cd072693f0fd562d67f6640c0d92becda1ee4
Miguel <40340920+pombcoraft@users.noreply.github.com>
2025-10-19 22:18:21 +0100

    feat: added mayor icon

--- src/overview/overview.gd
+++ src/overview/overview.gd
@@ -2,11 +2,12 @@ extends Node2D
 class_name Overview
 
 @onready var tabacomission: Button = %TabacoMission
-#@onready var presmission: Button = %PresMission
+@onready var presmission: Button = %MayorMission
 @onready var smog: Sprite2D = %Smog
 @onready var smogBad: Sprite2D = %SmogBad
 @onready var factoryWarning: Sprite2D = %FactoryWarning
 @onready var endWeek: TextureButton = %EndWeek
+@onready var presWarning: Sprite2D = %MayorWarning
 
 
 	# Button ditection is not working for som reason.
@@ -18,10 +19,10 @@ func _input(event):
 			var button_rect = Rect2(tabacomission.global_position, tabacomission.size)
 			if button_rect.has_point(event.global_position):
 				_handle_mission_click()
-#		if presmission.disabled == false:
-#			var button_rect = Rect2(presmission.global_position, presmission.size)
-#			if button_rect.has_point(event.global_position):
-#				_handle_mission_click()
+		if presmission.disabled == false:
+			var button_rect = Rect2(presmission.global_position, presmission.size)
+			if button_rect.has_point(event.global_position):
+				_handle_mission_click()
 		if endWeek.disabled == false:
 			var button_rect = Rect2(endWeek.global_position, endWeek.size)
 			if button_rect.has_point(event.global_position):
@@ -37,31 +38,39 @@ func _handle_mission_click():
 func _ready() -> void:
 	AudioManager.create_audio(SoundEffectSettings.SOUND_EFFECT_TYPE.OVERVIEW_MUSIC)
 	var tabacostate = GameManager.getLevelDecision(GameManager.LEVEL.TABACO)
+	var presstate = GameManager.getLevelDecision(GameManager.LEVEL.PRESIDENTE)
 	
-	#TEMPORARY:
-	if tabacostate != GameManager.DECISION_TYPE.TODO:
+
+	if presstate != GameManager.DECISION_TYPE.TODO: 
 		endWeek.visible = true
 	
 	
 	tabacomission.visible = false
-	#presmission.visible = true
+	presmission.visible = true
+	presWarning.visible = true
+	
 	match tabacostate:
 		GameManager.DECISION_TYPE.GOOD:
 			smog.visible = false
 			factoryWarning.visible=false
 			tabacomission.disabled = true
+			GameManager._tabaco_done = true
 		GameManager.DECISION_TYPE.TODO:
 			smog.visible = true
 			tabacomission.visible = true
-#			presmission.visible = false
+			presmission.visible = false
+			presWarning.visible = false
 			return
 		GameManager.DECISION_TYPE.BAD:
 			smog.visible = false
 			smogBad.visible = true
 			factoryWarning.visible=false
 			tabacomission.disabled = true
+			GameManager._tabaco_done = true
 		GameManager.DECISION_TYPE.MISS:
 			smog.visible = false
 			smogBad.visible = true
 			factoryWarning.visible = false
 			tabacomission.disabled = true
+			GameManager._tabaco_done = true
+			
